80a33e12fd814ca9231e5b86750efa031fc7c863
Dako Yu <dako@Dakos-MacBook-Air.local>
2023-02-13 22:24:14 -0500

    added font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
+import { ConfigProvider } from 'antd';
 import '../styles/main.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider
+            theme={{
+                token: {
+                    fontFamily: 'Ubuntu, sans-serif',
+                },
+            }}
+        >
+            <Component {...pageProps} />
+        </ConfigProvider>
+  )
+    
 }
